c02e736d8d11fb0aceee3873f2819fdd62fd40d0
ccc <169713467@qq.com>
2022-04-13 22:40:57 +0800

    测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node":true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": "latest",
+        "ecmaVersion": "2018",
         "sourceType": "module"
     },
     "rules": {
